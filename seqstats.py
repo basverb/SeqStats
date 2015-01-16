25ac1c58c2bc8d695628590758ee3a70fc75bfa6
Bas_Verbruggen <bv213@exeter.ac.uk>
2015-01-16 14:36:07 +0000

    added error for too few arguments

--- seqstats.py
+++ seqstats.py
@@ -4,19 +4,29 @@ Created on Fri Jan 16 11:33:12 2015
 
 @author: Bas Verbruggen
 """
+
 # Parse arguments
 from sys import argv
 from query_ncbi import *
 from fasta import *
 from gc_calculator import *
 
-query = argv[1]
-db = argv[2]
+def main(options):
+    # Check and process commandline arguments
+    assert len(options) == 3, "Provide at least two arguments: 1. gene query 2. database ('nucleotide' or 'protein')"
+    query = options[1]
+    db = options[2]
+    
+    # Run scripts
+    print '#1 query NCBI'
+    query_ncbi(query, db)
+    print '#2 parse fasta file'
+    sequences = fasta('query_result.fa')
+    print '#3 calculate gc content'
+    headers, gc_contents = calculate_all_gc(sequences)
+    print '#4 plot histogram'
+    plot_histogram(gc_contents, query)
 
-#query = 'vtg1'
-#db = 'nucleotide'
-# Run scripts
-query_ncbi(query, db)
-sequences = fasta('query_result.fa')
-headers, gc_contents = calculate_all_gc(sequences)
-plot_histogram(gc_contents, query)
+
+
+main(argv)
